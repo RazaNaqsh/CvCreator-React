4fe516ed40490a4c90746f510a7f14c0beddc341
RazaNaqsh <owaisraza1704@gmail.com>
2023-03-08 11:01:14 +0530

    Can preview typed content

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,50 @@
-import Form from "./components/Form";
+import React from "react";
 import Preview from "./components/Preview";
 
 function App() {
+	const [generalInfo, setGeneralInfo] = React.useState({
+		name: "",
+		email: "",
+		phone: "",
+	});
+
+	function handleChange(e) {
+		const { name, value } = e.target;
+		setGeneralInfo((prevData) => ({
+			...prevData,
+			[name]: value,
+		}));
+	}
+	console.log(generalInfo);
+
+	const generalInfoArr = ["name", "email", "phone"];
+	const generalInputElements = generalInfoArr.map((item) => {
+		return (
+			<label>
+				{item}{" "}
+				<input
+					type="text"
+					onChange={handleChange}
+					name={item}
+					value={generalInfo.item}
+				/>
+			</label>
+		);
+	});
 	return (
 		<div className="App">
-			<Form />
-			<Preview />
+			<div className="form-container">
+				<h1>Cv Maker</h1>
+				<section className="form-section">
+					<h2>Enter Your details</h2>
+					<div className="form-field-container">{generalInputElements}</div>
+
+					<h2>Enter your Educational Details</h2>
+					{/* {eduExpElements} */}
+				</section>
+			</div>
+
+			<Preview info={generalInfo} />
 		</div>
 	);
 }
--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
-export default function Preview() {
+export default function Preview(props) {
 	return (
 		<div className="preview-container">
 			<h1>Preview</h1>
-			<section className="preview-section"></section>
+			<section className="preview-section">
+				<h1>{props.info.name}</h1>
+				<p>{props.info.email}</p>
+				<p>{props.info.phone}</p>
+			</section>
 		</div>
 	);
 }
