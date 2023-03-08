fe4b58ab244271b1b893b7a983a6c85fddf0c783
RazaNaqsh <owaisraza1704@gmail.com>
2023-03-08 15:57:14 +0530

    Single state for managing edu field

--- src/App.jsx
+++ src/App.jsx
@@ -19,9 +19,9 @@ function App() {
 	// console.log(generalInfo);
 
 	const generalInfoArr = ["name", "email", "phone"];
-	const generalInputElements = generalInfoArr.map((item) => {
+	const generalInputElements = generalInfoArr.map((item, index) => {
 		return (
-			<label>
+			<label key={index}>
 				{item}
 				<input
 					type="text"
@@ -33,6 +33,43 @@ function App() {
 		);
 	});
 
+	const [eduInfo, setEduInfo] = React.useState([
+		{
+			id: nanoid(),
+			title: "",
+		},
+	]);
+	const eduElements = eduInfo.map((item) => (
+		<div
+			key={item.id}
+			className="form-field-container"
+		>
+			<label>
+				Title
+				<input
+					type="text"
+					onChange={() => handleEduChange(event, item.id)}
+					name="title"
+					value={item.title}
+				/>
+			</label>
+		</div>
+	));
+
+	function handleEduChange(e, id) {
+		console.log(id);
+		const { name, value } = e.target;
+		setEduInfo((prevData) => {
+			return prevData.map((item) =>
+				item.id === id ? { ...item, [name]: value } : item
+			);
+		});
+		console.log(eduInfo);
+	}
+
+	function newField() {
+		setEduInfo((prevData) => [...prevData, { id: nanoid(), title: "" }]);
+	}
 	return (
 		<div className="App">
 			<div className="form-container">
@@ -42,8 +79,8 @@ function App() {
 					<div className="form-field-container">{generalInputElements}</div>
 
 					<h2>Enter your Educational Details</h2>
-					{/* {eduExpElements} */}
-					{/* <button onClick={newField}>Add</button> */}
+					{eduElements}
+					<button onClick={newField}>Add</button>
 				</section>
 			</div>
 
---
+++ src/components/Input.jsx
@@ -0,0 +1,10 @@
+// import React from 'react'
+
+// export default function Input() {
+//   return (
+//       <input type={props.type}
+//           name={props.name}
+//           value
+//       />
+//   )
+// }
